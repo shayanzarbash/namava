c588ffd3469553098b6e5a7f9f1b5703589bdcbd
Shayan <shayan.zarbash@gmail.com>
2022-12-08 00:24:09 +0330

    Slider get info

--- src/components/slider/slider.js
+++ src/components/slider/slider.js
@@ -1,7 +1,18 @@
 import './Slider.scss';
 import SliderItem from './SliderItem';
+import axios from 'axios';
+import { useEffect } from 'react';
 
+const request = async () => {
+    const url = await axios.get("https://www.namava.ir/api/v1.0/medias/sliders/1316");
+    console.log(url['data']);
+}
 export const Slider = () => {
+
+    useEffect(() => {
+        request();
+    });
+
     return (
         <div className='col-12 p-0 slider'>
             <SliderItem />
