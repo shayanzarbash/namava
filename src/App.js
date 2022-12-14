e6a1e51ca27fa4b4826f5952f53e571a31e925a4
Shayan <shayan.zarbash@gmail.com>
2022-12-15 02:35:54 +0330

    add slider

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Home from './pages/Home';
 import './style.scss';
 import Provider from './utils/Provider';
+import 'flickity/dist/flickity.min.css';
 
 const App = () => {
   return (
--- src/components/list-movie/RowList.js
+++ src/components/list-movie/RowList.js
@@ -1,8 +1,38 @@
+import { createRef, useEffect } from 'react';
 import { Link } from 'react-router-dom';
 import MovieItem from '../MovieItem';
-import './RowList.scss'
+import './RowList.scss';
+import Flickity from 'flickity';
+
 
 const RowList = () => {
+
+    const flickityRef = createRef();
+
+    useEffect(() => {
+
+        let flickityHandler = undefined;
+        let elem = flickityRef.current.querySelector('.list');
+        if (flickityRef.current && elem) {
+            flickityHandler = new Flickity(elem, {
+                contain: true,
+                pageDots: false,
+                prevNextButtons: false,
+                cellAlign: 'right',
+                rightToLeft: true,
+                groupCells: true
+
+            });
+        }
+
+        return () => {
+            if (flickityHandler) {
+                flickityHandler.remove();
+            }
+        }
+
+    });
+
     return (
         <div className='row-list col-12 p-0'>
             <div className='row-title'>
@@ -11,15 +41,17 @@ const RowList = () => {
                     <span>مشاهده همه</span>
                 </Link>
             </div>
-            <div className='row-container'>
-                <div className='d-flex'>
+            <div className='list-container' ref={flickityRef}>
+                <div className='d-flex list'>
                     <MovieItem />
                     <MovieItem />
                     <MovieItem />
                     <MovieItem />
                     <MovieItem />
                     <MovieItem />
-                    
+                    <MovieItem />
+                    <MovieItem />
+                    <MovieItem />
 
                 </div>
             </div>
